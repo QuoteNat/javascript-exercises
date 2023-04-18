48fe311d04d78489f9f420e2fa043bac9760f072
QuoteNat <35079138+QuoteNat@users.noreply.github.com>
2023-04-18 15:51:39 -0500

    Implement convertToFahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -2,7 +2,8 @@ const convertToCelsius = function(fahrenheit) {
   return parseFloat((5/9 * (fahrenheit - 32)).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsius) {
+  return parseFloat(((celsius * 9 / 5) + 32).toFixed(1));
 };
 
 // Do not edit below this line
